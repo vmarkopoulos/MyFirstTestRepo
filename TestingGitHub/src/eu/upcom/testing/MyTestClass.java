fae576841205a3f428b4f31ab9a6d3c2a0bfcba7
vmarkopoulos <vmarkopoulos@upcom.eu>
2012-03-16 14:07:16 +0200

    Update TestingGitHub/src/eu/upcom/testing/MyTestClass.java

--- TestingGitHub/src/eu/upcom/testing/MyTestClass.java
+++ TestingGitHub/src/eu/upcom/testing/MyTestClass.java
@@ -3,6 +3,6 @@ package eu.upcom.testing;
 public class MyTestClass {
 
 	void myFirstOperation() {
-		System.out.println("This way I will force merging");
+		System.out.println("Create conflict before commit");
 	}
 }
