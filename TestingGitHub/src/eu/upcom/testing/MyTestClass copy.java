11671a355cd2bcc9b49d2880cc1d43801534a309
vmarkopoulos <vmarkopoulos@upcom.eu>
2015-09-15 15:01:43 +0300

    Update MyTestClass copy.java

--- TestingGitHub/src/eu/upcom/testing/MyTestClass copy.java
+++ TestingGitHub/src/eu/upcom/testing/MyTestClass copy.java
@@ -3,6 +3,6 @@ package eu.upcom.testing;
 public class MyTestClass {
 
 	void myFirstOperation() {
-		System.out.println("testing non conflict 1");
+		System.out.println("testing non conflict 2");
 	}
 }
